1f5c55459cfd74e77399072b5791a311c1122bc6
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-08-06 16:00:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline bg-gray-100">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
